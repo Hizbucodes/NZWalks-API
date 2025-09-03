c8aa3e2c2889ecdae40aed6bdff6c654ef54e562
Hizbucodes <hizbusoftware@gmail.com>
2025-09-03 22:56:34 +0530

    run ef core migrations to create identity database

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -15,7 +15,7 @@ namespace NZWalks.API.Data
             base.OnModelCreating(builder);
 
             var readerRoleId = "81ee1dda-becb-4550-9b5e-5599b54774cd";
-            var writerRoleId = "a34f2e24-ad56-4e5f-a6b1-c3d1a8fb9dc8"
+            var writerRoleId = "a34f2e24-ad56-4e5f-a6b1-c3d1a8fb9dc8";
 
             var roles = new List<IdentityRole>
             {
